040d1d1e4ef5fb33d117c5215f7024af2ffc3ff4
hucksy <hcksy.dev@gmail.com>
2022-05-19 12:54:51 -0500

    never ending config tweaks

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = f"{env_settings.DATABASE_TYPE}://{env_settings.DATABASE_USER}{env_settings.DATABASE_PASSWORD}:@{env_settings.DATABASE_HOST}/{env_settings.DATABASE_NAME}"
+SQLALCHEMY_DATABASE_URL = f"{env_settings.DATABASE_TYPE}://{env_settings.DATABASE_USER}:{env_settings.DATABASE_PASSWORD}@{env_settings.DATABASE_HOST}/{env_settings.DATABASE_NAME}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
